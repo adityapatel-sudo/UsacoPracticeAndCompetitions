2982ff87514dc68d6592f0134b248eaf56e6c9c3
adityapatel-sudo <adityapatel1204@gmail.com>
2021-11-03 11:54:14 -0700

    testing, getting back into usaco maybe

---
+++ src/GoldContestPrep/mainTest.java
@@ -0,0 +1,8 @@
+package GoldContestPrep;
+
+public class mainTest {
+    public static void main(String[] args){
+        System.out.println("im aditya patel");
+        System.out.println("i got a decent score on yang");
+    }
+}
